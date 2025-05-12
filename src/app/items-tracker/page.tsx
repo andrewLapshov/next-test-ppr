15df8d7ef19bac2b8d3ee4a50ab2fa840e0a11f1
Andrey Lapshov <lapshov.andrey4@wb.ru>
2025-05-12 15:47:48 +0300

    добавлена динамика (searchParams) в трекер

--- src/app/items-tracker/page.tsx
+++ src/app/items-tracker/page.tsx
@@ -2,17 +2,47 @@ import { fetchTotalItemsCached } from "./api";
 import { fetchDictionariesCached } from "infrastructure/graphql/api/dictionaries/api/fetch-dictionaries";
 import { fetchItemsTrackerListCached } from "./api/fetch-items-tracker-list";
 
+const FilterTypes = {
+  hideCompleted: "hideCompleted",
+  hideout: "hideout",
+  quests: "quests",
+} as const;
+
 export const revalidate = 86_400;
 export const dynamicParams = true;
 
-const ItemsTracker = async () => {
+type Props = {
+  searchParams: Promise<{
+    filters?: string;
+    search?: string;
+  }>;
+};
+
+const ItemsTracker = async ({ searchParams }: Props) => {
   await Promise.all([
     fetchDictionariesCached(),
     fetchTotalItemsCached(),
     fetchItemsTrackerListCached(),
   ]);
 
-  return <div>ItemsTracker</div>;
+  const { filters, search } = await searchParams;
+
+  const filterCategories = filters ? filters.split(",") : [];
+  const showQuests = filterCategories.includes(FilterTypes.quests);
+  const showHideout = filterCategories.includes(FilterTypes.hideout);
+  const hideCompleted = filterCategories.includes(FilterTypes.hideCompleted);
+
+  const searchValue = search?.toLowerCase();
+
+  return (
+    <div className={"flex flex-col gap-1"}>
+      ItemsTracker
+      <li>showQuests: {showQuests}</li>
+      <li>showHideout: {showHideout}</li>
+      <li>hideCompleted: {hideCompleted}</li>
+      <li>search: {searchValue}</li>
+    </div>
+  );
 };
 
 export default ItemsTracker;
