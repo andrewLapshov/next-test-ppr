7d6fdf16d88c1bcf3fb26d889fdcf0b063307d4b
Andrey Lapshov <lapshov.andrey4@wb.ru>
2025-05-30 12:51:09 +0300

    Кеширование gql

--- src/infrastructure/graphql/api/items/index.ts
+++ src/infrastructure/graphql/api/items/index.ts
@@ -1,18 +1,19 @@
 import { getClient } from "../../config";
 import { allItemsQuery } from "./all-items-query";
+import { Tags } from "../../tags";
 
 export const fetchAllItems = async (ids?: string[]) => {
   return getClient().query({
     query: allItemsQuery,
-    // context: {
-    //   fetchOptions: {
-    //     cache: "force-cache",
-    //     next: {
-    //       revalidate: 60 * 60 * 24, // 1 день
-    //       tags: [Tags.fetchAllItemsData],
-    //     },
-    //   },
-    // },
+    context: {
+      fetchOptions: {
+        cache: "force-cache",
+        next: {
+          revalidate: 60 * 60 * 24, // 1 день
+          tags: [Tags.fetchAllItemsData],
+        },
+      },
+    },
     variables: { lang: "ru", ids: ids },
   });
 };
